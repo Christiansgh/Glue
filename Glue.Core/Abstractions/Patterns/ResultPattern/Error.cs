e8e956518e56d1426e9014bb08da355ef07a2ae6
Arensbak <char@novax.dk>
2025-04-30 14:30:38 +0200

    feat: Add Result & ValueResult classes

---
+++ Glue.Core/Abstractions/Patterns/ResultPattern/Error.cs
@@ -0,0 +1,4 @@
+﻿namespace Glue.Core.Abstractions.Patterns.ResultPattern;
+public sealed record Error(string ErrorCode, string Description) {
+    public static readonly Error None = new(string.Empty, string.Empty);
+}
---
+++ Glue.Core/Abstractions/Patterns/ResultPattern/ValueResult.cs
@@ -0,0 +1,24 @@
+﻿namespace Glue.Core.Abstractions.Patterns.ResultPattern;
+
+public class ValueResult<T> {
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+    public T? Value { get; }
+    public static ValueResult<T> Success(T value) => new(true, Error.None, value);
+    public static ValueResult<T> Failure(Error error) => new(false, error, default);
+
+    private ValueResult(bool isSuccess, Error error, T? value) {
+        if (isSuccess && error != Error.None || !isSuccess && error == Error.None) {
+            throw new ArgumentException("Invalid error", nameof(error));
+        }
+
+        if (isSuccess && value is null) {
+            throw new ArgumentNullException(nameof(value), "Successfull results must have a value.");
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+        Value = value;
+    }
+}
